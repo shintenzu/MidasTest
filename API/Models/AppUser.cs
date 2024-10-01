e5c62f4a40e7575de3d348cd1e8eda23e169dc0f
Segun Olowofoyeku <oolowof1@students.kennesaw.edu>
2024-10-01 12:20:28 -0400

    first commit

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string? FullName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+
+    }
+}
